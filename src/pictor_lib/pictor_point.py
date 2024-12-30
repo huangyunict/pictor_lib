4f3ebe3d743e4534164fc4761b9c724d0e8862f4
Yun Huang <huangyunict@gmail.com>
2024-12-29 23:49:43 -0800

    rename PictorPosition to PictorPoint

--- src/pictor_lib/pictor_point.py
+++ src/pictor_lib/pictor_point.py
@@ -1,15 +1,15 @@
-"""Module that defines the PictorPosition class."""
+"""Module that defines the PictorPoint class."""
 from decimal import Decimal
 
 from src.pictor_lib.pictor_type import DecimalUnion
 from src.pictor_lib.pictor_size import PictorSize
 
 
-class PictorPosition(tuple[Decimal, Decimal]):
-    """Wrap 2d position (x, y)."""
+class PictorPoint(tuple[Decimal, Decimal]):
+    """Wrap 2d point (x, y)."""
 
     def __new__(cls, x: DecimalUnion = 0, y: DecimalUnion = 0):
-        return tuple.__new__(PictorPosition, (Decimal(x), Decimal(y)))
+        return tuple.__new__(PictorPoint, (Decimal(x), Decimal(y)))
 
     @property
     def x(self) -> Decimal:
@@ -29,27 +29,27 @@ class PictorPosition(tuple[Decimal, Decimal]):
 
         return round(self[0]), round(self[1])
 
-    def set_x(self, x: DecimalUnion) -> 'PictorPosition':
+    def set_x(self, x: DecimalUnion) -> 'PictorPoint':
         """Set the x property and return a new instance."""
 
-        return PictorPosition(x, self[1])
+        return PictorPoint(x, self[1])
 
-    def set_y(self, y: DecimalUnion) -> 'PictorPosition':
+    def set_y(self, y: DecimalUnion) -> 'PictorPoint':
         """Set the y property and return a new instance."""
 
-        return PictorPosition(self[0], y)
+        return PictorPoint(self[0], y)
 
-    def move(self, offset: PictorSize) -> 'PictorPosition':
+    def move(self, offset: PictorSize) -> 'PictorPoint':
         """Return a new instance by moving the given offset."""
 
-        return PictorPosition(self.x + offset.width, self.y + offset.height)
+        return PictorPoint(self.x + offset.width, self.y + offset.height)
 
-    def move_x(self, offset: DecimalUnion) -> 'PictorPosition':
+    def move_x(self, offset: DecimalUnion) -> 'PictorPoint':
         """Return a new instance by moving the x field of given offset."""
 
-        return PictorPosition(self.x + offset, self.y)
+        return PictorPoint(self.x + offset, self.y)
 
-    def move_y(self, offset: DecimalUnion) -> 'PictorPosition':
+    def move_y(self, offset: DecimalUnion) -> 'PictorPoint':
         """Return a new instance by moving the y field of given offset."""
 
-        return PictorPosition(self.x, self.y + offset)
+        return PictorPoint(self.x, self.y + offset)
